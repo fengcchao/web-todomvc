a658cef4a387eb585da62bf2a637cad31d2a0e7b
sapphire36 <18829343079@163.com>
2018-05-23 18:08:52 +0800

    first commit

---
+++ src/component/TodoTextInput.js
@@ -0,0 +1,25 @@
+import React, { Component ,ProTypes} from 'react'
+import classnames from 'classnames'
+export default class TodoTextInput extends Component {
+  handleSubmit=e=>{
+    const text =e.target.value.trim()
+    if(e.which==13){
+      this.props.onSave(text)
+    }
+  }
+
+
+  render() {
+    return (
+     <input 
+     type="text"
+     autoFocus="ture"
+     placeholder="What needs to be done?"
+     className='new-todo'
+     onKeyDown={this.handleSubmit}
+     />
+
+    );
+  }
+}
+
